06de84735a3e502877c6c8293f91b0e5c276b686
Pikatchu99 <yemalin.agbangla@epitech.eu>
2021-08-01 06:38:07 +0100

    add code

---
+++ ruby_apk/f_data.rb
@@ -0,0 +1,12 @@
+require "cgi"
+
+cgi = CGI.new
+cgi.out("type" => "text/html", "charset" => "UTF-8") {
+    get = cgi['falseValue']
+    "<html>
+      <body>
+        <p>受け取ったStringは下記になります</p>
+        <p>String：#{get}</p>
+      </body>
+    </html>"
+}
